db782c8aa8623de843aab6714d8a67f86ed3b1b8
faxter <info@faxxter.com>
2026-02-24 18:08:34 +0100

    create function for cleaning user input

---
+++ repl/repl.go
@@ -0,0 +1,11 @@
+package repl
+
+import "strings"
+
+func cleanInput(text string) []string {
+	words := strings.Fields(text)
+	for i, word := range words {
+		words[i] = strings.ToLower(word)
+	}
+	return words
+}
---
+++ repl/repl_test.go
@@ -0,0 +1,37 @@
+package repl
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  white\tspace   ",
+			expected: []string{"white", "space"},
+		},
+		{
+			input:    "Some dIfFeRenT CASES",
+			expected: []string{"some", "different", "cases"},
+		},
+		{
+			input:    "Battle-Armor",
+			expected: []string{"battle-armor"},
+		},
+	}
+
+	for _, testCase := range cases {
+		actual := cleanInput(testCase.input)
+		if len(testCase.expected) != len(actual) {
+			t.Errorf("expected %d words, but got %d", len(testCase.expected), len(actual))
+		}
+		for i := range actual {
+			word := actual[i]
+			expectedWord := testCase.expected[i]
+			if word != expectedWord {
+				t.Errorf("expected: %s, got: %s", expectedWord, word)
+			}
+		}
+	}
+}
