69818ab15e85738925b4d80e119f0d1462c58bcd
faxter <info@faxxter.com>
2026-03-01 22:06:53 +0100

    move map commands to new file

--- repl/commands.go
+++ repl/commands.go
@@ -8,10 +8,6 @@ import (
 	"github.com/faxter/pokefx/internal/pokeapi"
 )
 
-const (
-	API_MAP_BASE = "https://pokeapi.co/api/v2/location-area/"
-)
-
 func (r *Repl) RegisterCommands() {
 	r.registerCommand("exit", "Exit the program", r.commandExit)
 	r.registerCommand("help", "Display usage information", r.commandHelp)
@@ -27,6 +23,18 @@ func (r *Repl) registerCommand(name, description string, callback func(*Config, 
 	}
 }
 
+func (r *Repl) ExecuteCommand(command string, param string) {
+	cmd, ok := r.commands[command]
+	if !ok {
+		fmt.Println("unknown command")
+	} else {
+		err := cmd.callback(&r.config, param)
+		if err != nil {
+			fmt.Println("error: ", err)
+		}
+	}
+}
+
 func (r *Repl) commandExit(cfg *Config, _ string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
@@ -38,48 +46,6 @@ func (r *Repl) commandHelp(cfg *Config, _ string) error {
 	fmt.Println("Usage:")
 	for name, cmd := range r.commands {
 		fmt.Printf("\t%s:\t%s\n", name, cmd.description)
-	}
-	return nil
-}
-
-func (r *Repl) commandMap(cfg *Config, param string) error {
-	if param != "" {
-		return r.exploreSpecificMap(param)
-	}
-	var url string
-	if cfg.NextPage != "" {
-		url = cfg.NextPage
-	} else {
-		url = API_MAP_BASE + "?offset=0&limit=20"
-	}
-	data, err := r.fetchRawData(url)
-	if err != nil {
-		return err
-	}
-	response, err := decode[pokeapi.MapListResponse](data)
-	if err != nil {
-		return err
-	}
-	for _, name := range response.ExtractMapNames() {
-		fmt.Println(name)
-	}
-	cfg.NextPage = response.Next
-	cfg.PreviousPage = response.Previous
-	return nil
-}
-
-func (r *Repl) exploreSpecificMap(mapname string) error {
-	url := API_MAP_BASE + mapname
-	data, err := r.fetchRawData(url)
-	if err != nil {
-		return err
-	}
-	response, err := decode[pokeapi.SpecificMapResponse](data)
-	if err != nil {
-		return err
-	}
-	for _, pokemon := range response.ExtractPokemonEncounters() {
-		fmt.Println(pokemon)
 	}
 	return nil
 }
@@ -101,39 +67,4 @@ func decode[T any](raw []byte) (T, error) {
 	var jsonData T
 	err := json.Unmarshal(raw, &jsonData)
 	return jsonData, err
-}
-
-func (r *Repl) commandMapBack(cfg *Config, _ string) error {
-	var url string
-	if cfg.PreviousPage != "" {
-		url = cfg.PreviousPage
-	} else {
-		return fmt.Errorf("you're on the first page")
-	}
-	data, err := r.fetchRawData(url)
-	if err != nil {
-		return err
-	}
-	response, err := decode[pokeapi.MapListResponse](data)
-	if err != nil {
-		return err
-	}
-	for _, name := range response.ExtractMapNames() {
-		fmt.Println(name)
-	}
-	cfg.NextPage = response.Next
-	cfg.PreviousPage = response.Previous
-	return nil
-}
-
-func (r *Repl) ExecuteCommand(command string, param string) {
-	cmd, ok := r.commands[command]
-	if !ok {
-		fmt.Println("unknown command")
-	} else {
-		err := cmd.callback(&r.config, param)
-		if err != nil {
-			fmt.Println("error: ", err)
-		}
-	}
 }
---
+++ repl/mapCommands.go
@@ -0,0 +1,76 @@
+package repl
+
+import (
+	"fmt"
+
+	"github.com/faxter/pokefx/internal/pokeapi"
+)
+
+const (
+	API_MAP_BASE = "https://pokeapi.co/api/v2/location-area/"
+)
+
+func (r *Repl) commandMap(cfg *Config, param string) error {
+	if param != "" {
+		return r.exploreSpecificMap(param)
+	}
+	var url string
+	if cfg.NextPage != "" {
+		url = cfg.NextPage
+	} else {
+		url = API_MAP_BASE + "?offset=0&limit=20"
+	}
+	data, err := r.fetchRawData(url)
+	if err != nil {
+		return err
+	}
+	response, err := decode[pokeapi.MapListResponse](data)
+	if err != nil {
+		return err
+	}
+	for _, name := range response.ExtractMapNames() {
+		fmt.Println(name)
+	}
+	cfg.NextPage = response.Next
+	cfg.PreviousPage = response.Previous
+	return nil
+}
+
+func (r *Repl) exploreSpecificMap(mapname string) error {
+	url := API_MAP_BASE + mapname
+	data, err := r.fetchRawData(url)
+	if err != nil {
+		return err
+	}
+	response, err := decode[pokeapi.SpecificMapResponse](data)
+	if err != nil {
+		return err
+	}
+	for _, pokemon := range response.ExtractPokemonEncounters() {
+		fmt.Println(pokemon)
+	}
+	return nil
+}
+
+func (r *Repl) commandMapBack(cfg *Config, _ string) error {
+	var url string
+	if cfg.PreviousPage != "" {
+		url = cfg.PreviousPage
+	} else {
+		return fmt.Errorf("you're on the first page")
+	}
+	data, err := r.fetchRawData(url)
+	if err != nil {
+		return err
+	}
+	response, err := decode[pokeapi.MapListResponse](data)
+	if err != nil {
+		return err
+	}
+	for _, name := range response.ExtractMapNames() {
+		fmt.Println(name)
+	}
+	cfg.NextPage = response.Next
+	cfg.PreviousPage = response.Previous
+	return nil
+}
